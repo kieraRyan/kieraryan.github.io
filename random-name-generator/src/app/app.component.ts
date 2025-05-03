eb698e1c6abb450868b944d8ed750db7cd6a1040
kieraRyan <kierar260@gmail.com>
2025-05-03 16:58:09 +0100

    Allow user to input teams and display below

--- random-name-generator/src/app/app.component.ts
+++ random-name-generator/src/app/app.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { Team } from './models/team';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatInputModule, MatFormFieldModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'random-name-generator';
+  
+  teams: Team[] = [];
+  enteredTeamMembers: string = "";
+
+
+  addNewTeam(event: any) : void {
+    console.log(this.enteredTeamMembers);
+    this.teams.push({
+      teamName: this.getNextTeamName(),
+      members: this.enteredTeamMembers.split(',')
+    });
+    console.log('You have added a new team');
+  }
+  
+  getNextTeamName() : string {
+    return `Team ${this.teams.length + 1}`;
+  }
 }
