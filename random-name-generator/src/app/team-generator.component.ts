07f0dd5df567025d1fa925331fcbf62c5d05f3a2
kieraRyan <kierar260@gmail.com>
2025-05-07 13:19:36 +0100

    Add team number selection drop down

--- random-name-generator/src/app/team-generator.component.ts
+++ random-name-generator/src/app/team-generator.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import { Team } from './models/team';
@@ -9,7 +10,8 @@ import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'team-generator-component',
-  imports: [RouterOutlet, MatInputModule, MatFormFieldModule, FormsModule, NgFor, NgIf],
+  imports: [RouterOutlet, MatInputModule, MatFormFieldModule, MatSelectModule, FormsModule, NgFor, NgIf
+  ],
   templateUrl: './team-generator.component.html',
   styleUrl: './app.component.css',
 })
@@ -19,5 +21,12 @@ export class TeamGeneratorComponent {
 
   displayTeams: boolean = false;
   enteredTeamMembers: string = "";
+
+  teamNumOptions: number[] = Array.from({length: 10}, (_, i) => i + 1)
+  selectedTeamNum: number = 1;
+
+  onTeamNumberChange(event: any): void {
+    this.displayTeams = false;
+  }
 
 }
