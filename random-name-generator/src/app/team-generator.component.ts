73e6ef3702b1d6cd9ff6d82c0e2e7b9627ad79d9
kieraRyan <kierar260@gmail.com>
2025-05-07 16:56:19 +0100

    Add ability to randomly generate teams if no input data provided

--- random-name-generator/src/app/team-generator.component.ts
+++ random-name-generator/src/app/team-generator.component.ts
@@ -17,32 +17,99 @@ import { NgFor, NgIf } from '@angular/common';
 })
 export class TeamGeneratorComponent {
 
-  teams: Team[] = [];
+  inputTeams: Team[] = [];
+  teamsToDisplay: Team[] = [];
 
-  displayTeams: boolean = false;
   enteredTeamMembers: string = "";
+  teamMemberArray: string[] = [];
 
-  teamNumOptions: number[] = Array.from({ length: 10 }, (_, i) => i + 1)
+  teamNumOptions: number[] = Array.from({ length: 10 }, (_, i) => i + 1);
   selectedTeamNum: number = 1;
+
+  errorMessage: string = '';
 
   constructor(private router: Router) {
     const nav = this.router.getCurrentNavigation();
     const stateData = nav?.extras.state?.['inputTeams'];
 
     if (stateData) {
-      this.teams = stateData;
+      this.inputTeams = stateData;
       return;
     }
 
     // if not in state, check session storage
     const saved = sessionStorage.getItem('inputTeams');
     if (saved) {
-      this.teams = JSON.parse(saved);
+      this.inputTeams = JSON.parse(saved);
     }
   }
 
   onTeamNumberChange(event: any): void {
-    this.displayTeams = false;
+    this.teamsToDisplay = [];
+    this.errorMessage = "";
+  }
+
+  validateTeamLength(): boolean {
+    if (this.teamMemberArray.length < this.selectedTeamNum) {
+      return false;
+    }
+    return true;
+  }
+
+  displayRandomTeams(event: any): void {
+    this.teamsToDisplay = [];
+    this.errorMessage = "";
+    this.teamMemberArray = this.enteredTeamMembers.split(',');
+
+    if (!this.validateTeamLength()) {
+      this.errorMessage = `You have not entered enough people to make ${this.selectedTeamNum} team(s)`;
+      return;
+    }
+
+    // if predefined teams provided, use those
+    if (this.inputTeams.length !== 0) {
+      this.teamsToDisplay = this.inputTeams;
+      return;
+    }
+
+    this.teamsToDisplay = this.generateRandomTeams();
+  
+  }
+
+  private generateRandomTeams(): Team[] {
+    let teamsList: Team[] = this.constructEmptyTeamArray();
+    while (this.assignMembersToTeams(teamsList)){
+      continue;
+    }
+    return teamsList;
+  }
+
+  private assignMembersToTeams(teamsList: Team[]): boolean {
+    for (let i = 0; i < teamsList.length; i++) {
+      if (this.teamMemberArray.length === 0){
+        return false;
+      }
+      
+      let index = Math.floor(Math.random() * this.teamMemberArray.length);
+      let selectedMember = this.teamMemberArray[index];
+
+      teamsList[i].members.push(selectedMember);
+      this.teamMemberArray.splice(index, 1);
+
+    }
+
+    return true;
+  }
+
+  private constructEmptyTeamArray(): Team[] {
+    let emptyTeams: Team[] = [];
+    for (let i = 1; i <= this.selectedTeamNum; i++) {
+      emptyTeams.push( {
+        teamName: `Team ${i}`,
+        members: []
+      });
+    }
+    return emptyTeams;
   }
 
 }
